b84aef52595645d177adfc99444907501f1a00e8
bharat Sarvi <bharasarvi@gmail.com>
2022-04-08 19:21:04 +0530

    add field and errormessage component

--- src/components/YoutubeForm.js
+++ src/components/YoutubeForm.js
@@ -1,4 +1,4 @@
-import { Form, Formik } from "formik";
+import { ErrorMessage, Field, Form, Formik } from "formik";
 import React from "react";
 import * as Yup from "yup";
 
@@ -19,7 +19,6 @@ const validationSchema = Yup.object({
 });
 
 const YoutubeForm = () => {
-  console.log("Visiter Field: ", formik.touched);
   return (
     <Formik
       initialValues={initialValues}
@@ -29,41 +28,20 @@ const YoutubeForm = () => {
       <Form>
         <div className="form-control">
           <label htmlFor="name">Name</label>
-          <input
-            type="text"
-            id="name"
-            name="name"
-            {...formik.getFieldProps("name")}
-          />
-          {formik.touched.name && formik.errors.name ? (
-            <div className="error">{formik.errors.name}</div>
-          ) : null}
+          <Field type="text" id="name" name="name" />
+          <ErrorMessage name="name" />
         </div>
 
         <div className="form-control">
           <label htmlFor="email">Email</label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            {...formik.getFieldProps("email")}
-          />
-          {formik.touched.email && formik.errors.email ? (
-            <div className="error">{formik.errors.email}</div>
-          ) : null}
+          <Field type="email" id="email" name="email" />
+          <ErrorMessage name="email" />
         </div>
 
         <div className="form-control">
           <label htmlFor="channel">Channel</label>
-          <input
-            type="text"
-            id="channel"
-            name="channel"
-            {...formik.getFieldProps("channel")}
-          />
-          {formik.touched.channel && formik.errors.channel ? (
-            <div className="error">{formik.errors.channel}</div>
-          ) : null}
+          <Field type="text" id="channel" name="channel" />
+          <ErrorMessage name="channel" />
         </div>
 
         <button type="submit">Submit</button>
