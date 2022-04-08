220f1ff37956f7c2aec495dc973e904c19b5f4ca
bharat Sarvi <bharasarvi@gmail.com>
2022-04-08 20:50:05 +0530

    add array to form

--- src/components/YoutubeForm.js
+++ src/components/YoutubeForm.js
@@ -12,6 +12,7 @@ const initialValues = {
     facebook: "",
     twitter: "",
   },
+  phoneNumbers: ["", ""],
 };
 
 const onSubmit = (values) => {
@@ -71,6 +72,18 @@ const YoutubeForm = () => {
           <label htmlFor="twitter">Twitter</label>
           <Field type="text" id="twitter" name="social.twitter" />
           <ErrorMessage name="social.twitter" component={TextError} />
+        </div>
+
+        <div className="=form-control">
+          <label htmlFor="primaryPh">Primary Phone Number</label>
+          <Field type="text" id="primaryPh" name="phoneNumbers[0]" />
+          <ErrorMessage name="phoneNumbers[0]" component={TextError} />
+        </div>
+
+        <div className="=form-control">
+          <label htmlFor="secondaryPh">Seconday Phone Number</label>
+          <Field type="text" id="secondaryPh" name="phoneNumbers[1]" />
+          <ErrorMessage name="phoneNumbers[1]" component={TextError} />
         </div>
 
         <button type="submit">Submit</button>
