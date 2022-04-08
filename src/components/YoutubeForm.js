19ac58c9a48ef8a527eb4dc08c710466fb7bcc37
bharat Sarvi <bharasarvi@gmail.com>
2022-04-08 18:09:05 +0530

    add validation

--- src/components/YoutubeForm.js
+++ src/components/YoutubeForm.js
@@ -1,18 +1,39 @@
 import { useFormik } from "formik";
 import React from "react";
 
+const initialValues = {
+  name: "",
+  email: "",
+  channel: "",
+};
+
+const onSubmit = (values) => {
+  console.log("Form Data:", values);
+};
+
+const validate = (values) => {
+  let errors = {};
+  if (!values.name) {
+    errors.name = "Required";
+  }
+  if (!values.email) {
+    errors.email = "Required";
+  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+    errors.email = "Invalid email format";
+  }
+  if (!values.channel) {
+    errors.channel = "Required";
+  }
+  return errors;
+};
+
 const YoutubeForm = () => {
   const formik = useFormik({
-    initialValues: {
-      name: "",
-      email: "",
-      channel: "",
-    },
-    onSubmit: (values) => {
-      console.log("Form Data:", values);
-    },
+    initialValues,
+    onSubmit,
+    validate,
   });
-  //   console.log("Form values: ", formik.values);
+  console.log("Form errors: ", formik.errors);
   return (
     <div>
       <form onSubmit={formik.handleSubmit}>
